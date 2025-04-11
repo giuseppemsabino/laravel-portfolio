d3b0955452821949b9a8f8a4ee5c2e894f98a5a0
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-12 00:10:12 +0200

    xd

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profolio</title>
+    <title>Protfolio</title>
 </head>
 <body>
     <p>{{$user->name}}</p>
