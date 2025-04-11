6414d0ee95cc3801e086c40513b0b4ecabfee32b
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-12 00:09:41 +0200

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profolio</title>
+</head>
+<body>
+    <p>{{$user->name}}</p>
+    <p>{{$user->email}}</p>
+    <p>{{$user->id}}</p>
+</body>
+</html>
