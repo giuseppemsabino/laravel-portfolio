dae87eafe4e8a4b4b867485f27621753a1fb4b61
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-12 20:51:52 +0200

    posts tabel

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for( $i = 0; $i < 10; $i++) {
+           $newPost = new Post();
+           
+            $newPost->title = $faker->sentence();
+            $newPost->author = $faker->name();
+            $newPost->category = $faker->word();
+            $newPost->content = $faker->paragraphs(3, true);
+
+            $newPost->save();
+           
+        }
+    }
+}
