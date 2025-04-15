4a820f47cdcaf95b5a084426a494280e91bf7ba4
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-15 23:29:20 +0200

    technologies table added

---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $technologies = [
+            "PHP",
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "React",
+            "Lavarel",
+            "Blade",
+            "SCSS"
+        ];
+
+        foreach($technologies as $technology){
+            $newTechnology = new Technology();
+
+            $newTechnology->name = $technology;
+            $newTechnology->color = $faker->hexColor();
+
+            $newTechnology->save();
+        }
+    }
+}
